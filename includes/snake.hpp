956867e3df718d94ebbdcffbddda458380f50650
Jeremie Joannard <Samael@localhost.localdomain>
2022-04-04 17:21:32 +0200

    [ADD]setup

---
+++ includes/snake.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2022
+** Untitled (Workspace)
+** File description:
+** snake
+*/
+
+#ifndef SNAKE_HPP_
+#define SNAKE_HPP_
+
+class snake {
+    public:
+    protected:
+    private:
+};
+
+#endif /* !SNAKE_HPP_ */
